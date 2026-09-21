f44fd760d4627093c4dbe21ca63e76656834cee7
MOHAMED YUNUS A <mohamedyunus.f5coders@gmail.com>
2026-09-21 12:44:21 +0530

    Updates

---
+++ sw.js
@@ -0,0 +1,41 @@
+// Dream Catchers Foundation - MAHALAKSHMI S Service Worker
+const CACHE_NAME = "dream-catchers-mahalakshmi-v1";
+
+const ASSETS_TO_CACHE = [
+  "./",
+  "index.html",
+  "style.css",
+  "script.js",
+  "manifest.json",
+  "mahalakshmi.vcf",
+  "assets/dream-catchers-logo.jpg",
+  "assets/MAHA.PNG"
+];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS_TO_CACHE).catch(() => {});
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k))
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((cachedResponse) => {
+      return cachedResponse || fetch(e.request).catch(() => cachedResponse);
+    })
+  );
+});
